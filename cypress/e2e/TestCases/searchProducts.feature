d413f95ea133e0182d0015ef6f6b8e36ef91d202
Felipe Ferreira <felipeferreira.qa@gmail.com>
2024-08-15 10:31:21 -0300

    fix: corrigindo lógica

--- cypress/e2e/TestCases/searchProducts.feature
+++ cypress/e2e/TestCases/searchProducts.feature
@@ -5,3 +5,5 @@ Feature: Api Testing
         When I send a request to search the products
         Then The response status should be 200
         And The response body must have only the product that I search
+        And The category must be MICE
+        And The product name must be Microsoft Sculpt Touch Mouse
--- cypress/e2e/TestCases/searchProducts/searchProducts.js
+++ cypress/e2e/TestCases/searchProducts/searchProducts.js
@@ -24,4 +24,12 @@ describe("Get a Product", () => {
     And("The response body must have only the product that I search", () => {
         expect(response.body).to.have.lengthOf(1);
     });
+
+    And("The category must be MICE", () => {
+        expect(response.body[0]).to.have.property("categoryName").eq("MICE")
+    });
+
+    And("The product name must be Microsoft Sculpt Touch Mouse", () =>{
+        expect(response.body[0].products[0]).to.have.property("productName").eq("Microsoft Sculpt Touch Mouse")
+    });
 });
